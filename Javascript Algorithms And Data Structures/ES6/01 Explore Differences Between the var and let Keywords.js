f72f9b9347fb89a24a11fbf415d74c556e66d1a0
Dennis <deweis@gmx.ch>
2018-06-01 18:32:04 +0200

    Add Explore Differences Between the var and let Keywords

---
+++ Javascript Algorithms And Data Structures/ES6/01 Explore Differences Between the var and let Keywords.js
@@ -0,0 +1,16 @@
+/**
+* Task: Update the code so it only uses the let keyword.
+*/
+
+var catName;
+var quote;
+
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+
+catTalk();
