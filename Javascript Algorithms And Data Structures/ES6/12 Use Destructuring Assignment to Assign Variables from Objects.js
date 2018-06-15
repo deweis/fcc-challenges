701e778f1ba733ed4fa44a721a64afdf1abde287
Dennis <deweis@gmx.ch>
2018-06-15 12:04:44 +0200

    Add "Use Destructuring Assignment to Assign Variables from Objects"

---
+++ Javascript Algorithms And Data Structures/ES6/12 Use Destructuring Assignment to Assign Variables from Objects.js
@@ -0,0 +1,16 @@
+/**
+* Task: Use destructuring to obtain the length of the input string str, and
+* assign the length to len in line.
+*/
+function getLength(str) {
+  "use strict";
+
+  // change code below this line
+  const length = 0; // change this
+  // change code above this line
+
+  return len; // you must assign length to len in line
+
+}
+
+console.log(getLength('FreeCodeCamp'));
