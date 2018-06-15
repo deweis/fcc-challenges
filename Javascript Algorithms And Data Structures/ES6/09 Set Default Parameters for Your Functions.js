35024eb68c983e26f5211b41eec23f3e4fc3b72c
Dennis <deweis@gmx.ch>
2018-06-15 11:53:29 +0200

    Add "Set Default Parameters for Your Functions"

---
+++ Javascript Algorithms And Data Structures/ES6/09 Set Default Parameters for Your Functions.js
@@ -0,0 +1,13 @@
+/**
+* Task: Modify the function increment by adding default parameters so that it
+* will add 1 to number if value is not specified.
+*/
+const increment = (function() {
+  "use strict";
+  return function increment(number, value) {
+    return number + value;
+  };
+})();
+
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns NaN
