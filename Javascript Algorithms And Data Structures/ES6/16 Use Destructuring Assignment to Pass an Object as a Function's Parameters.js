73860eb1b46eed7de2b3bf3d9f39014cad40f6a7
Dennis <deweis@gmx.ch>
2018-06-17 12:32:50 +0200

    Solve "Use Destructuring Assignment to Pass an Object as a Function's Parameters"

--- Javascript Algorithms And Data Structures/ES6/16 Use Destructuring Assignment to Pass an Object as a Function's Parameters.js
+++ Javascript Algorithms And Data Structures/ES6/16 Use Destructuring Assignment to Pass an Object as a Function's Parameters.js
@@ -14,9 +14,9 @@ const half = (function() {
   "use strict"; // do not change this line
 
   // change code below this line
-  return function half(stats) {
+  return function half( {max, min}) {
     // use function argument destructuring
-    return (stats.max + stats.min) / 2.0;
+    return (max + min) / 2.0;
   };
   // change code above this line
 
