27b30a98c47bd8507a0c59e7c421588fbef438cd
Dennis <deweis@gmx.ch>
2018-06-19 11:33:46 +0200

    Add "Use * to Import Everything from a File"

---
+++ Javascript Algorithms And Data Structures/ES6/23 Use star to Import Everything from a File.js
@@ -0,0 +1,6 @@
+/**
+* Task: The code below requires the contents of a file, "capitalize_strings",
+* found in the same directory as it, imported. Add the appropriate import *
+* statement to the top of the file, using the object provided.
+*/
+"use strict";
