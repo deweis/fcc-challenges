e22ccb1a5ba85e129777a818b08688cbf9417c50
Dennis <deweis@gmx.ch>
2018-06-19 11:37:31 +0200

    Add "Import a Default Export"

---
+++ Javascript Algorithms And Data Structures/ES6/25 Import a Default Export.js
@@ -0,0 +1,6 @@
+/**
+* Task: In the following code, please import the default export, subtract, from
+* the file "math_functions", found in the same directory as this file.
+*/
+"use strict";
+subtract(7,4);
