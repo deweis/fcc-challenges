e436d5e6e495a3917f8fb8cc411624ad7eb42b85
Dennis <deweis@gmx.ch>
2018-06-19 10:58:45 +0200

    Solve "Write Concise Object Literal Declarations Using Simple Fields"

--- Javascript Algorithms And Data Structures/ES6/17 Write Concise Object Literal Declarations Using Simple Fields.js
+++ Javascript Algorithms And Data Structures/ES6/17 Write Concise Object Literal Declarations Using Simple Fields.js
@@ -5,9 +5,9 @@ const createPerson = (name, age, gender) => {
   "use strict";
   // change code below this line
   return {
-    name: name,
-    age: age,
-    gender: gender
+    name,
+    age,
+    gender
   };
   // change code above this line
 };
