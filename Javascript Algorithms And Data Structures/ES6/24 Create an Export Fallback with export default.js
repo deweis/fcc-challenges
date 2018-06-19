f6abb0b658a30c9aa21e83f350adcfd0ee8394b5
Dennis <deweis@gmx.ch>
2018-06-19 11:35:31 +0200

    Add "Create an Export Fallback with export default"

---
+++ Javascript Algorithms And Data Structures/ES6/24 Create an Export Fallback with export default.js
@@ -0,0 +1,6 @@
+/**
+* Task: The following function should be the fallback value for the module.
+* Please add the necessary code to do so.
+*/
+"use strict";
+function subtract(x,y) {return x - y;}
