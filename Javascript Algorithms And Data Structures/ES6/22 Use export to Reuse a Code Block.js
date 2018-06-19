6ba89720fa341183976044c2221926d2cc7c9fe9
Dennis <deweis@gmx.ch>
2018-06-19 11:29:17 +0200

    Add "Use export to Reuse a Code Block"

---
+++ Javascript Algorithms And Data Structures/ES6/22 Use export to Reuse a Code Block.js
@@ -0,0 +1,7 @@
+/**
+* Task: Below are two variables that I want to make available for other files to
+* use. Utilizing the first way I demonstrated export, export the two variables.
+*/
+"use strict";
+const foo = "bar";
+const boo = "far";
