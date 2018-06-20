969c5b3abaa22e1328c51bb09f0146aa47c1cc6d
Dennis <deweis@gmx.ch>
2018-06-20 09:45:35 +0200

    Add "Using the Test Method"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/01 Using the Test Method.js
@@ -0,0 +1,6 @@
+/**
+* Task: Apply the regex myRegex on the string myString using the .test() method.
+*/
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex; // Change this line
