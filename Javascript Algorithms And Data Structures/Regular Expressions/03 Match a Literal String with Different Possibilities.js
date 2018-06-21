9fa450d479e4620966814341271dddd175babe38
Dennis <deweis@gmx.ch>
2018-06-21 11:08:29 +0200

    Add "Match a Literal String with Different Possibilities"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/03 Match a Literal String with Different Possibilities.js
@@ -0,0 +1,7 @@
+/**
+* Task: Complete the regex petRegex to match the pets "dog", "cat", "bird",
+* or "fish".
+*/
+let petString = "James has a pet cat.";
+let petRegex = /change/; // Change this line
+let result = petRegex.test(petString);
