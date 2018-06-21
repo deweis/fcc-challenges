f70a31efb49cfafdba11c8ea1ac436c1ed17d70b
Dennis <deweis@gmx.ch>
2018-06-21 11:18:24 +0200

    Add "Find More Than the First Match"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/06 Find More Than the First Match.js
@@ -0,0 +1,9 @@
+/**
+* Task: Using the regex starRegex, find and extract both "Twinkle" words from
+* the string twinkleStar.
+*
+* Note: You can have multiple flags on your regex like /search/gi
+*/
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /change/; // Change this line
+let result = twinkleStar; // Change this line
