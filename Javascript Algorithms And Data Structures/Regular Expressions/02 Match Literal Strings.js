b4732d183b8fd54d5d6ee3f38aae34070a367c36
Dennis <deweis@gmx.ch>
2018-06-21 11:06:25 +0200

    Add "Match Literal Strings"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/02 Match Literal Strings.js
@@ -0,0 +1,7 @@
+/**
+* Task: Complete the regex waldoRegex to find "Waldo" in the string waldoIsHiding
+* with a literal match.
+*/
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /search/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
