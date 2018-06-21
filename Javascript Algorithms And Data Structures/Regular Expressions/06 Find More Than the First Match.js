aba94e107424398a8e71bfc2d2b014be94ee4857
Dennis <deweis@gmx.ch>
2018-06-21 11:19:40 +0200

    Solve "Find More Than the First Match"

--- Javascript Algorithms And Data Structures/Regular Expressions/06 Find More Than the First Match.js
+++ Javascript Algorithms And Data Structures/Regular Expressions/06 Find More Than the First Match.js
@@ -5,5 +5,5 @@
 * Note: You can have multiple flags on your regex like /search/gi
 */
 let twinkleStar = "Twinkle, twinkle, little star";
-let starRegex = /change/; // Change this line
-let result = twinkleStar; // Change this line
+let starRegex = /twinkle/gi; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line
