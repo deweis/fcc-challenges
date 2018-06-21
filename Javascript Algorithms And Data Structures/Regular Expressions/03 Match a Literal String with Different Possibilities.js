8bdfeb248b6b1074d3ac77c50a42fdc2009e8edf
Dennis <deweis@gmx.ch>
2018-06-21 11:09:11 +0200

    Solve "Match a Literal String with Different Possibilities"

--- Javascript Algorithms And Data Structures/Regular Expressions/03 Match a Literal String with Different Possibilities.js
+++ Javascript Algorithms And Data Structures/Regular Expressions/03 Match a Literal String with Different Possibilities.js
@@ -3,5 +3,5 @@
 * or "fish".
 */
 let petString = "James has a pet cat.";
-let petRegex = /change/; // Change this line
+let petRegex = /dog|cat|bird|fish/; // Change this line
 let result = petRegex.test(petString);
