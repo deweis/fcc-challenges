03810f420f38f74f2588f251393e5cac38af6e0d
Dennis <deweis@gmx.ch>
2018-06-21 11:14:23 +0200

    Add "Extract Matches"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/05 Extract Matches.js
@@ -0,0 +1,6 @@
+/**
+* Task: Apply the .match() method to extract the word coding.
+*/
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /change/; // Change this line
+let result = extractStr; // Change this line
