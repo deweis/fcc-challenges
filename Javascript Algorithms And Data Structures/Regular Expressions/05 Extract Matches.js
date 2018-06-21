69874d23106dfeae7b46fdafef0652fc29bc00ea
Dennis <deweis@gmx.ch>
2018-06-21 11:16:25 +0200

    Solve "Extract Matches"

--- Javascript Algorithms And Data Structures/Regular Expressions/05 Extract Matches.js
+++ Javascript Algorithms And Data Structures/Regular Expressions/05 Extract Matches.js
@@ -2,5 +2,5 @@
 * Task: Apply the .match() method to extract the word coding.
 */
 let extractStr = "Extract the word 'coding' from this string.";
-let codingRegex = /change/; // Change this line
-let result = extractStr; // Change this line
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
