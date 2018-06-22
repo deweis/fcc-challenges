236f9fd734dd6ceb4fe667a2b9810434521b6c53
Dennis <deweis@gmx.ch>
2018-06-22 09:57:13 +0200

    Solve "Match Numbers and Letters of the Alphabet"

--- Javascript Algorithms And Data Structures/Regular Expressions/10 Match Numbers and Letters of the Alphabet.js
+++ Javascript Algorithms And Data Structures/Regular Expressions/10 Match Numbers and Letters of the Alphabet.js
@@ -4,5 +4,5 @@
 * flags in the regex.
 */
 let quoteSample = "Blueberry 3.141592653s are delicious.";
-let myRegex = /change/; // Change this line
-let result = myRegex; // Change this line
+let myRegex = /[h-s2-6]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
