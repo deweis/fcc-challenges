556876b4a3fe5500ff40cee9e396e847d48c8871
Dennis <deweis@gmx.ch>
2018-06-22 09:54:06 +0200

    Solve "Match Letters of the Alphabet"

--- Javascript Algorithms And Data Structures/Regular Expressions/09 Match Letters of the Alphabet.js
+++ Javascript Algorithms And Data Structures/Regular Expressions/09 Match Letters of the Alphabet.js
@@ -2,5 +2,5 @@
 * Task: Match all the letters in the string quoteSample.
 */
 let quoteSample = "The quick brown fox jumps over the lazy dog.";
-let alphabetRegex = /change/; // Change this line
-let result = alphabetRegex; // Change this line
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
