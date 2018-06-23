25264a1a76612e78cd768bd8a0aaa72fc9155455
Dennis <deweis@gmx.ch>
2018-06-23 12:32:51 +0200

    Add "Match Characters that Occur Zero or More Times"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/13 Match Characters that Occur Zero or More Times.js
@@ -0,0 +1,8 @@
+/**
+* Task: Create a regex chewieRegex that uses the * character to match all the
+* upper and lower"a" characters in chewieQuote. Your regex does not need flags,
+* and it should not match any of the other quotes.
+*/
+let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+let chewieRegex = /change/; // Change this line
+let result = chewieQuote.match(chewieRegex);
