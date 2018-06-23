9110696f58e269a8f3ab30d2015c450e5c125ce7
Dennis <deweis@gmx.ch>
2018-06-23 12:29:50 +0200

    Add "Match Characters that Occur One or More Times"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/12 Match Characters that Occur One or More Times.js
@@ -0,0 +1,7 @@
+/**
+* Task: You want to find matches when the letter s occurs one or more times in
+* "Mississippi". Write a regex that uses the + sign.
+*/
+let difficultSpelling = "Mississippi";
+let myRegex = /change/; // Change this line
+let result = difficultSpelling.match(myRegex);
