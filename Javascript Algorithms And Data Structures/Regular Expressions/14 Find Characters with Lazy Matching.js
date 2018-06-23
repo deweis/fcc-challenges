07843a280bf7749a2062b36bbfa2831694f6895c
Dennis <deweis@gmx.ch>
2018-06-23 12:41:06 +0200

    Solve "Find Characters with Lazy Matching"

--- Javascript Algorithms And Data Structures/Regular Expressions/14 Find Characters with Lazy Matching.js
+++ Javascript Algorithms And Data Structures/Regular Expressions/14 Find Characters with Lazy Matching.js
@@ -4,5 +4,5 @@
 * matches any character.
 */
 let text = "<h1>Winter is coming</h1>";
-let myRegex = /<.*>/; // Change this line
+let myRegex = /<.*?>/; // Change this line
 let result = text.match(myRegex);
