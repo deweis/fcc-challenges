246317f3f045af97ba421f6d5fc917bb07545d6d
Dennis <deweis@gmx.ch>
2018-06-24 10:16:13 +0200

    Add "Match All Letters and Numbers"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/18 Match All Letters and Numbers.js
@@ -0,0 +1,7 @@
+/**
+* Task: Use the shorthand character class \w to count the number of alphanumeric
+* characters in various quotes and strings.
+*/
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /change/; // Change this line
+let result = quoteSample.match(alphabetRegexV2).length;
