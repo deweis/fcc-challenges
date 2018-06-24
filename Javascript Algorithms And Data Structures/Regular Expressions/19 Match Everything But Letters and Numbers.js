e463813349173ba67e95aa9bbb10dd2fba51596c
Dennis <deweis@gmx.ch>
2018-06-24 10:20:34 +0200

    Add "Match Everything But Letters and Numbers"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/19 Match Everything But Letters and Numbers.js
@@ -0,0 +1,7 @@
+/**
+* Task: Use the shorthand character class \W to count the number of non-alphanumeric
+* characters in various quotes and strings.
+*/
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /change/; // Change this line
+let result = quoteSample.match(nonAlphabetRegex).length;
