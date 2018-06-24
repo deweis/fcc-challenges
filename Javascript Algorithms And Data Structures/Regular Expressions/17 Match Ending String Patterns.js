ba01b8dfe3edd45dba4dafc35c4b720a472da52d
Dennis <deweis@gmx.ch>
2018-06-24 10:15:25 +0200

    Solve "Match Ending String Patterns"

--- Javascript Algorithms And Data Structures/Regular Expressions/17 Match Ending String Patterns.js
+++ Javascript Algorithms And Data Structures/Regular Expressions/17 Match Ending String Patterns.js
@@ -3,5 +3,5 @@
 * of the string caboose.
 */
 let caboose = "The last car on a train is the caboose";
-let lastRegex = /change/; // Change this line
+let lastRegex = /caboose$/; // Change this line
 let result = lastRegex.test(caboose);
