daf10d0d2a0adbb526ea5b789c2f1ca814f9454a
Dennis <deweis@gmx.ch>
2018-06-25 11:06:02 +0200

    Add "Restrict Possible Usernames"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/22 Restrict Possible Usernames.js
@@ -0,0 +1,6 @@
+/**
+* Task: Change the regex userCheck to fit the constraints listed above.
+*/
+let username = "JackOfAllTrades";
+let userCheck = /change/; // Change this line
+let result = userCheck.test(username);
