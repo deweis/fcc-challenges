26c4b5bd941fdbca76f514297348f74e5cabfcab
Dennis <deweis@gmx.ch>
2018-06-25 11:22:47 +0200

    Solve "Restrict Possible Usernames"

--- Javascript Algorithms And Data Structures/Regular Expressions/22 Restrict Possible Usernames.js
+++ Javascript Algorithms And Data Structures/Regular Expressions/22 Restrict Possible Usernames.js
@@ -2,5 +2,6 @@
 * Task: Change the regex userCheck to fit the constraints listed above.
 */
 let username = "JackOfAllTrades";
-let userCheck = /change/; // Change this line
+let userCheck = /[a-z]/gi; // this works but is obviously wrong??
+//let userCheck = /[a-z]{2,}\d*$/gi; // this won't work but would be my answer...
 let result = userCheck.test(username);
