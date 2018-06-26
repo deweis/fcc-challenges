434f6d8e67f76686b8868abb8765a2079f0088e1
Dennis <deweis@gmx.ch>
2018-06-26 11:25:55 +0200

    Solve "Match Non-Whitespace Characters"

--- Javascript Algorithms And Data Structures/Regular Expressions/24 Match Non-Whitespace Characters.js
+++ Javascript Algorithms And Data Structures/Regular Expressions/24 Match Non-Whitespace Characters.js
@@ -3,5 +3,5 @@
 * characters in a string.
 */
 let sample = "Whitespace is important in separating words";
-let countNonWhiteSpace = /change/; // Change this line
+let countNonWhiteSpace = /\S/g; // Change this line
 let result = sample.match(countNonWhiteSpace);
