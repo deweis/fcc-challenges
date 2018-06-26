7791c299984dd79dd2f1cd25dc5104c430199800
Dennis <deweis@gmx.ch>
2018-06-26 11:25:15 +0200

    Add "Match Non-Whitespace Characters"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/24 Match Non-Whitespace Characters.js
@@ -0,0 +1,7 @@
+/**
+* Task: Change the regex countNonWhiteSpace to look for multiple non-whitespace
+* characters in a string.
+*/
+let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /change/; // Change this line
+let result = sample.match(countNonWhiteSpace);
