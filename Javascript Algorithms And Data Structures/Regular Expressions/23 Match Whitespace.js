fd85de0dd8b9fb7a0e89fd070007330e2fca465c
Dennis <deweis@gmx.ch>
2018-06-26 11:23:39 +0200

    Add "Match Whitespace"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/23 Match Whitespace.js
@@ -0,0 +1,7 @@
+/**
+* Task: Change the regex countWhiteSpace to look for multiple whitespace
+* characters in a string.
+*/
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /change/; // Change this line
+let result = sample.match(countWhiteSpace);
