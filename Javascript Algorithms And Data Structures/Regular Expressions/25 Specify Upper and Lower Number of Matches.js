edc952341f8ee81adf09a923c5e16c2c40cf3d1d
Dennis <deweis@gmx.ch>
2018-06-26 11:26:37 +0200

    Add "Specify Upper and Lower Number of Matches"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/25 Specify Upper and Lower Number of Matches.js
@@ -0,0 +1,7 @@
+/**
+* Task: Change the regex countNonWhiteSpace to look for multiple non-whitespace
+* characters in a string.
+*/
+let ohStr = "Ohhh no";
+let ohRegex = /change/; // Change this line
+let result = ohRegex.test(ohStr);
