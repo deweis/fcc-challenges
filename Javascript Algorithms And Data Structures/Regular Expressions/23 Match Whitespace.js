e351b2f336db209e4b0542a31153bec6d044319c
Dennis <deweis@gmx.ch>
2018-06-26 11:24:30 +0200

    Solve "Match Whitespace"

--- Javascript Algorithms And Data Structures/Regular Expressions/23 Match Whitespace.js
+++ Javascript Algorithms And Data Structures/Regular Expressions/23 Match Whitespace.js
@@ -3,5 +3,5 @@
 * characters in a string.
 */
 let sample = "Whitespace is important in separating words";
-let countWhiteSpace = /change/; // Change this line
+let countWhiteSpace = /\s/g; // Change this line
 let result = sample.match(countWhiteSpace);
