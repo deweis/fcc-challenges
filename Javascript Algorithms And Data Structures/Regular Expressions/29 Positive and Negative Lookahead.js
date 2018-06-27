c6e4ba2eb24863ab7f161280424237765b0cf9a3
Dennis <deweis@gmx.ch>
2018-06-27 12:10:38 +0200

    Solve "Positive and Negative Lookahead"

--- Javascript Algorithms And Data Structures/Regular Expressions/29 Positive and Negative Lookahead.js
+++ Javascript Algorithms And Data Structures/Regular Expressions/29 Positive and Negative Lookahead.js
@@ -3,5 +3,5 @@
 * 5 characters long and have two consecutive digits.
 */
 let sampleWord = "astronaut";
-let pwRegex = /change/; // Change this line
+let pwRegex = /(?=\w{6,})(?=\D*\d{2})/; // Change this line
 let result = pwRegex.test(sampleWord);
