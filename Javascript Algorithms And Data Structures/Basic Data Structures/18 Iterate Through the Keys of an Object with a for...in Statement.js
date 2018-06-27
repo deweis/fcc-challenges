15d212d78763c84fb28a2c32fabd05fceb141687
Dennis <deweis@gmx.ch>
2018-06-27 12:50:13 +0200

    Add "Iterate Through the Keys of an Object with a for...in Statement"

---
+++ Javascript Algorithms And Data Structures/Basic Data Structures/18 Iterate Through the Keys of an Object with a for...in Statement.js
@@ -0,0 +1,30 @@
+/**
+* Task: We've defined a function, countOnline; use a for...in statement within
+* this function to loop through the users in the users object and return the number of users whose online property is set to true.
+*/
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function countOnline(obj) {
+  // change code below this line
+
+  // change code above this line
+}
+
+console.log(countOnline(users));
