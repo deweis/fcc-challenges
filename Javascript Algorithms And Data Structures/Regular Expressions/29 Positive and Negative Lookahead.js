270ec656d50013afd2d47b1976713fc8d2ff124c
Dennis <deweis@gmx.ch>
2018-06-27 11:51:47 +0200

    Add "Positive and Negative Lookahead"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/29 Positive and Negative Lookahead.js
@@ -0,0 +1,7 @@
+/**
+* Task: Use lookaheads in the pwRegex to match passwords that are greater than
+* 5 characters long and have two consecutive digits.
+*/
+let sampleWord = "astronaut";
+let pwRegex = /change/; // Change this line
+let result = pwRegex.test(sampleWord);
