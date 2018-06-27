cdc83601240614215776fbe76d4b9a49b6a659e2
Dennis <deweis@gmx.ch>
2018-06-27 11:43:14 +0200

    Add "Specify Only the Lower Number of Matches"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/26 Specify Only the Lower Number of Matches.js
@@ -0,0 +1,7 @@
+/**
+* Task: Change the regex haRegex to match the word "Hazzah" only when it has
+* four or more letter z's.
+*/
+let haStr = "Hazzzzah";
+let haRegex = /change/; // Change this line
+let result = haRegex.test(haStr);
