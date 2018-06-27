d4286fc47047daede103f0015a4f05b69675383a
Dennis <deweis@gmx.ch>
2018-06-27 11:47:10 +0200

    Add "Specify Exact Number of Matches"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/27 Specify Exact Number of Matches.js
@@ -0,0 +1,7 @@
+/**
+* Task: Change the regex timRegex to match the word "Timber" only when it has
+* four letter m's.
+*/
+let timStr = "Timmmmber";
+let timRegex = /change/; // Change this line
+let result = timRegex.test(timStr);
