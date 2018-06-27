5ae618b34967139fda8245395a45adde09edbcc8
Dennis <deweis@gmx.ch>
2018-06-27 11:49:00 +0200

    Add "Check for All or None"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/28 Check for All or None.js
@@ -0,0 +1,7 @@
+/**
+* Task: Change the regex favRegex to match both the American English (favorite)
+* and the British English (favourite) version of the word.
+*/
+let favWord = "favorite";
+let favRegex = /change/; // Change this line
+let result = favRegex.test(favWord);
