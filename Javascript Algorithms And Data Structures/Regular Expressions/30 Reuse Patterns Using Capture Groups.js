8a7656ae96630c5bf5b53735b39173ab2ce53289
Dennis <deweis@gmx.ch>
2018-06-28 13:01:20 +0200

    Add "Reuse Patterns Using Capture Groups"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/30 Reuse Patterns Using Capture Groups.js
@@ -0,0 +1,7 @@
+/**
+* Task: Use capture groups in reRegex to match numbers that are repeated only
+* three times in a string, each separated by a space.
+*/
+let repeatNum = "42 42 42";
+let reRegex = /change/; // Change this line
+let result = reRegex.test(repeatNum);
