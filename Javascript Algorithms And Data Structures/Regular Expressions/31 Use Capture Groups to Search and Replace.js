6cbd9c453a6fa7473800ca9164cfa4f3a3303be1
Dennis <deweis@gmx.ch>
2018-06-29 10:43:12 +0200

    Add "Use Capture Groups to Search and Replace"

---
+++ Javascript Algorithms And Data Structures/Regular Expressions/31 Use Capture Groups to Search and Replace.js
@@ -0,0 +1,8 @@
+/**
+* Task: Write a regex so that it will search for the string "good". Then update
+* the replaceText variable to replace "good" with "okey-dokey".
+*/
+let huhText = "This sandwich is good.";
+let fixRegex = /change/; // Change this line
+let replaceText = ""; // Change this line
+let result = huhText.replace(fixRegex, replaceText);
