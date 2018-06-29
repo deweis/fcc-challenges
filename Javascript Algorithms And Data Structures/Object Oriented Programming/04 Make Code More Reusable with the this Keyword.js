fd0fc4d5a5c0fcb2b6b64a6b97fa54016ea62c38
Dennis <deweis@gmx.ch>
2018-06-29 11:14:20 +0200

    Solve "Make Code More Reusable with the this Keyword"

--- Javascript Algorithms And Data Structures/Object Oriented Programming/04 Make Code More Reusable with the this Keyword.js
+++ Javascript Algorithms And Data Structures/Object Oriented Programming/04 Make Code More Reusable with the this Keyword.js
@@ -5,7 +5,7 @@
 let dog = {
   name: "Spot",
   numLegs: 4,
-  sayLegs: function() {return "This dog has " + dog.numLegs + " legs.";}
+  sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
 };
 
 dog.sayLegs();
