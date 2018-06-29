1aee5903714c267476af2b88cb10b32e84ed2c2b
Dennis <deweis@gmx.ch>
2018-06-29 11:08:33 +0200

    Add "Create a Method on an Object"

---
+++ Javascript Algorithms And Data Structures/Object Oriented Programming/03 Create a Method on an Object.js
@@ -0,0 +1,11 @@
+/**
+* Task: Using the dog object, give it a method called sayLegs. The method should
+* return the sentence "This dog has 4 legs."
+*/
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+
+};
+
+dog.sayLegs();
