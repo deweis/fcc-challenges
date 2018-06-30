8b774d5656960a3fdb818fda96b5c69ccf00e311
Dennis <deweis@gmx.ch>
2018-06-30 14:32:06 +0200

    Solve "Define a Constructor Function"

--- Javascript Algorithms And Data Structures/Object Oriented Programming/05 Define a Constructor Function.js
+++ Javascript Algorithms And Data Structures/Object Oriented Programming/05 Define a Constructor Function.js
@@ -2,3 +2,8 @@
 * Task: Create a constructor, Dog, with properties name, color, and numLegs that
 * are set to a string, a string, and a number, respectively.
 */
+function Dog() {
+  this.name = "dog";
+  this.color = "blue";
+  this.numLegs = 3;
+}
