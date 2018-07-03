ef7131321d1fb08c31a533eae1f7c1c8aac059e9
Dennis <deweis@gmx.ch>
2018-07-03 10:14:21 +0200

    Solve "Understand the Constructor Property"

--- Javascript Algorithms And Data Structures/07 Object Oriented Programming/12 Understand the Constructor Property.js
+++ Javascript Algorithms And Data Structures/07 Object Oriented Programming/12 Understand the Constructor Property.js
@@ -9,5 +9,9 @@ function Dog(name) {
 
 // Add your code below this line
 function joinDogFraternity(candidate) {
-
+    if (candidate.constructor === Dog) {
+    return true;
+  } else {
+    return false;
+  }
 }
