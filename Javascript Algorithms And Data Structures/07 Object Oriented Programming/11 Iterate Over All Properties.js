b6d5a51599de8f7939b95f5328b95210dcc5a491
Dennis <deweis@gmx.ch>
2018-07-03 10:05:32 +0200

    Add "Iterate Over All Properties"

---
+++ Javascript Algorithms And Data Structures/07 Object Oriented Programming/11 Iterate Over All Properties.js
@@ -0,0 +1,15 @@
+/**
+* Task: Add a numLegs property to the prototype of Dog
+*/
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Add your code below this line 
