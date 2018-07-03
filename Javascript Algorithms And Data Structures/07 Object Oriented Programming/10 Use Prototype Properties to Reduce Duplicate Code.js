4dca3713109782983805c74dcf7b8bd8ff6b3c8b
Dennis <deweis@gmx.ch>
2018-07-03 10:02:34 +0200

    Add "Use Prototype Properties to Reduce Duplicate Code"

---
+++ Javascript Algorithms And Data Structures/07 Object Oriented Programming/10 Use Prototype Properties to Reduce Duplicate Code.js
@@ -0,0 +1,11 @@
+/**
+* Task: Add a numLegs property to the prototype of Dog
+*/
+function Dog(name) {
+  this.name = name;
+}
+
+
+
+// Add your code above this line
+let beagle = new Dog("Snoopy");
