1dba30d80502f701aa9f39b95c499e862344aabd
Dennis <deweis@gmx.ch>
2018-07-05 10:36:40 +0200

    Solve "Understand the Prototype Chain"

--- Javascript Algorithms And Data Structures/07 Object Oriented Programming/16 Understand the Prototype Chain.js
+++ Javascript Algorithms And Data Structures/07 Object Oriented Programming/16 Understand the Prototype Chain.js
@@ -10,4 +10,4 @@ let beagle = new Dog("Snoopy");
 Dog.prototype.isPrototypeOf(beagle);  // => true
 
 // Fix the code below so that it evaluates to true
-???.isPrototypeOf(Dog.prototype);
+Object.prototype.isPrototypeOf(Dog.prototype);
