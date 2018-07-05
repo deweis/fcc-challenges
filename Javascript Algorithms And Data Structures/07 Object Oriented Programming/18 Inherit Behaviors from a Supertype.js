df03e0764e9f90e1e4bacb9cf40d6cdc6e78f3bc
Dennis <deweis@gmx.ch>
2018-07-05 10:41:28 +0200

    Add "Inherit Behaviors from a Supertype"

---
+++ Javascript Algorithms And Data Structures/07 Object Oriented Programming/18 Inherit Behaviors from a Supertype.js
@@ -0,0 +1,19 @@
+/**
+* Task: Use Object.create to make two instances of Animal named duck and beagle.
+*/
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Add your code below this line
+
+let duck; // Change this line
+let beagle; // Change this line
+
+duck.eat(); // Should print "nom nom nom"
+beagle.eat(); // Should print "nom nom nom" 
