559d2902d447bbbac5e4ffcf7470a44e6897053b
Dennis <deweis@gmx.ch>
2018-07-06 11:44:58 +0200

    Solve "Reset an Inherited Constructor Property"

--- Javascript Algorithms And Data Structures/07 Object Oriented Programming/20 Reset an Inherited Constructor Property.js
+++ Javascript Algorithms And Data Structures/07 Object Oriented Programming/20 Reset an Inherited Constructor Property.js
@@ -10,8 +10,8 @@ Bird.prototype = Object.create(Animal.prototype);
 Dog.prototype = Object.create(Animal.prototype);
 
 // Add your code below this line
-
-
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
 
 let duck = new Bird();
 let beagle = new Dog();
