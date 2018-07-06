ac1f3716d8bfb9af90e5ef195e4e34a8385900ba
Dennis <deweis@gmx.ch>
2018-07-06 11:48:37 +0200

    Solve "Add Methods After Inheritance"

--- Javascript Algorithms And Data Structures/07 Object Oriented Programming/21 Add Methods After Inheritance.js
+++ Javascript Algorithms And Data Structures/07 Object Oriented Programming/21 Add Methods After Inheritance.js
@@ -10,10 +10,12 @@ Animal.prototype.eat = function() { console.log("nom nom nom"); };
 function Dog() { }
 
 // Add your code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
 
-
-
-
+Dog.prototype.bark = function() {
+  console.log("Woof!");
+};
 // Add your code above this line
 
 let beagle = new Dog();
