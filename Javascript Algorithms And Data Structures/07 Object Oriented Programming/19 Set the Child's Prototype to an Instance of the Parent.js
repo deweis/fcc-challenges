8dcdea63695527b64b586181f7920a161f969b89
Dennis <deweis@gmx.ch>
2018-07-06 11:41:16 +0200

    Add "Set the Child's Prototype to an Instance of the Parent"

---
+++ Javascript Algorithms And Data Structures/07 Object Oriented Programming/19 Set the Child's Prototype to an Instance of the Parent.js
@@ -0,0 +1,19 @@
+/**
+* Task:Modify the code so that instances of Dog inherit from Animal.
+*/
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+// Add your code below this line
+
+
+let beagle = new Dog();
+beagle.eat();  // Should print "nom nom nom"
