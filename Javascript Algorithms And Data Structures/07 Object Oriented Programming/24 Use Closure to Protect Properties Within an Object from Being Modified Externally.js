ae8e528f44d18c41994def655f4147d80ea9abcf
Dennis <deweis@gmx.ch>
2018-07-07 13:21:55 +0200

    Add "Use Closure to Protect Properties Within an Object from Being Modified Externally"

---
+++ Javascript Algorithms And Data Structures/07 Object Oriented Programming/24 Use Closure to Protect Properties Within an Object from Being Modified Externally.js
@@ -0,0 +1,9 @@
+/**
+* Task: Change how weight is declared in the Bird function so it is a private
+" variable. Then, create a method getWeight that returns the value of weight.
+*/
+function Bird() {
+  this.weight = 15;
+
+
+}
