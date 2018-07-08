4b91af10e6423da8b8d19c867afbc1c2ab29756c
Dennis <deweis@gmx.ch>
2018-07-08 11:37:11 +0200

    Solve "Understand the Immediately Invoked Function Expression (IIFE)"

--- Javascript Algorithms And Data Structures/07 Object Oriented Programming/25 Understand the Immediately Invoked Function Expression (IIFE).js
+++ Javascript Algorithms And Data Structures/07 Object Oriented Programming/25 Understand the Immediately Invoked Function Expression (IIFE).js
@@ -2,8 +2,6 @@
 * Task: Rewrite the function makeNest and remove its call so instead it's an
 * anonymous immediately invoked function expression (IIFE).
 */
-function makeNest() {
+(function() {
   console.log("A cozy nest is ready");
-}
-
-makeNest();
+})();
