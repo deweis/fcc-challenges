6469272046a372ef1c1c20c3bd8b334f46983e0b
Dennis <deweis@gmx.ch>
2018-07-08 11:38:03 +0200

    Add "Use an IIFE to Create a Module"

---
+++ Javascript Algorithms And Data Structures/07 Object Oriented Programming/26 Use an IIFE to Create a Module.js
@@ -0,0 +1,14 @@
+/**
+* Task: Create a module named funModule to wrap the two mixins isCuteMixin and
+* singMixin. funModule should return an object.
+*/
+let isCuteMixin = function(obj) {
+  obj.isCute = function() {
+    return true;
+  };
+};
+let singMixin = function(obj) {
+  obj.sing = function() {
+    console.log("Singing to an awesome tune");
+  };
+};
