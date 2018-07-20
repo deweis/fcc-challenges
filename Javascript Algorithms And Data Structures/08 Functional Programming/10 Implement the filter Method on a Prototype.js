cdb00cc22866ee3b788ae5de664ca0ba45ac7312
Dennis <deweis@gmx.ch>
2018-07-20 10:55:53 +0200

    Solve "Implement the filter Method on a Prototype"

--- Javascript Algorithms And Data Structures/08 Functional Programming/10 Implement the filter Method on a Prototype.js
+++ Javascript Algorithms And Data Structures/08 Functional Programming/10 Implement the filter Method on a Prototype.js
@@ -9,6 +9,11 @@ var s = [23, 65, 98, 5];
 Array.prototype.myFilter = function(callback){
   var newArray = [];
   // Add your code below this line
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i])) {
+      newArray.push(this[i]);
+    }
+  }
 
   // Add your code above this line
   return newArray;
