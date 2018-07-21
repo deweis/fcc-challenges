efe55e68591036883cede51c1aa137a0f256bfc3
Dennis <deweis@gmx.ch>
2018-07-21 12:20:14 +0200

    Add "Combine Two Arrays Using the concat Method"

---
+++ Javascript Algorithms And Data Structures/08 Functional Programming/13 Combine Two Arrays Using the concat Method.js
@@ -0,0 +1,13 @@
+/**
+* Task: Use the concat method in the nonMutatingConcat function to concatenate
+* attach to the end of original. The function should return the concatenated array.
+*/
+function nonMutatingConcat(original, attach) {
+  // Add your code below this line
+
+
+  // Add your code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingConcat(first, second);
