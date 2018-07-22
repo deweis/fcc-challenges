e5738251a81f07b4495ea024556bb1fbe10cafa9
Dennis <deweis@gmx.ch>
2018-07-22 10:24:05 +0200

    Solve "Return a Sorted Array Without Changing the Original Array"

--- Javascript Algorithms And Data Structures/08 Functional Programming/17 Return a Sorted Array Without Changing the Original Array.js
+++ Javascript Algorithms And Data Structures/08 Functional Programming/17 Return a Sorted Array Without Changing the Original Array.js
@@ -6,7 +6,7 @@
 var globalArray = [5, 6, 3, 2, 9];
 function nonMutatingSort(arr) {
   // Add your code below this line
-
+  return [].concat(arr).sort( (a,b) => a - b);
 
   // Add your code above this line
 }
