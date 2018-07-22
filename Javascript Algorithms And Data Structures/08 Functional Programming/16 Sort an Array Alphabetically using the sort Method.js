787dc3638ef55243f378108aa9ba86bf8e8d8ca2
Dennis <deweis@gmx.ch>
2018-07-22 10:17:44 +0200

    Add "Sort an Array Alphabetically using the sort Method"

---
+++ Javascript Algorithms And Data Structures/08 Functional Programming/16 Sort an Array Alphabetically using the sort Method.js
@@ -0,0 +1,11 @@
+/**
+* Task: Use the sort method in the alphabeticalOrder function to sort the
+* elements of arr in alphabetical order.
+*/
+function alphabeticalOrder(arr) {
+  // Add your code below this line
+
+
+  // Add your code above this line
+}
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
