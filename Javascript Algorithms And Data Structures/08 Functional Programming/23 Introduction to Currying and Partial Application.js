d242dd2d8810ccb1d71f32d0517d17cfabe77b34
Dennis <deweis@gmx.ch>
2018-07-23 10:29:14 +0200

    Add "Introduction to Currying and Partial Application"

---
+++ Javascript Algorithms And Data Structures/08 Functional Programming/23 Introduction to Currying and Partial Application.js
@@ -0,0 +1,11 @@
+/**
+* Task: Fill in the body of the add function so it uses currying to add
+* parameters x, y, and z.
+*/
+function add(x) {
+  // Add your code below this line
+
+
+  // Add your code above this line
+}
+add(10)(20)(30);
