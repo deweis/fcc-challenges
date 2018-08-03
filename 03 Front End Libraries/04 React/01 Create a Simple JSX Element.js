aedc9b5d9101c42150436688a6745f893c4f6db0
Dennis <deweis@gmx.ch>
2018-08-03 14:34:23 +0200

    Solve "Create a Simple JSX Element"

--- 03 Front End Libraries/04 React/01 Create a Simple JSX Element.js
+++ 03 Front End Libraries/04 React/01 Create a Simple JSX Element.js
@@ -2,4 +2,4 @@
 * Task: The current code uses JSX to assign a div element to the constant JSX.
 * Replace the div with an h1 element and add the text Hello JSX! inside it.
 */
-const JSX = <div></div>;
+const JSX = <h1>Hello JSX!</h1>;
