9b55cafae68d537424305ae30b85f26a4cab86b5
Dennis <deweis@gmx.ch>
2018-08-03 14:36:56 +0200

    Add "Create a Complex JSX Element"

---
+++ 03 Front End Libraries/04 React/02 Create a Complex JSX Element.js
@@ -0,0 +1,8 @@
+/**
+* Task: Define a new constant JSX that renders a div which contains the following
+* elements in order:
+*
+* An h1, a p, and an unordered list that contains three li items. You can include
+* any text you want within each element.
+*/
+// write your code here
