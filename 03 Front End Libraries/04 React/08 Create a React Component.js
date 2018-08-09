7575c0e665bfcc9518997b7053956848c614157d
Dennis <deweis@gmx.ch>
2018-08-09 09:58:10 +0200

    Add "Create a React Component"

---
+++ 03 Front End Libraries/04 React/08 Create a React Component.js
@@ -0,0 +1,17 @@
+/**
+* Task: MyComponent is defined in the code editor using class syntax. Finish
+* writing the render method so it returns a div element that contains an h1 with
+* the text Hello React!.
+*/
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // change code below this line
+
+
+
+    // change code above this line
+  }
+};
