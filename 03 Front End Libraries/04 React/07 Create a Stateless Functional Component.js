b2f2568d2c3873aab9366f9b6d64772172fe40b5
Dennis <deweis@gmx.ch>
2018-08-09 09:54:12 +0200

    Add "Create a Stateless Functional Component"

---
+++ 03 Front End Libraries/04 React/07 Create a Stateless Functional Component.js
@@ -0,0 +1,14 @@
+/**
+* Task: The code editor has a function called MyComponent. Complete this
+*function so it returns a single div element which contains some string of text.
+
+Note: The text is considered a child of the div element, so you will not be able
+*     to use a self-closing tag.
+*/
+const MyComponent = function() {
+  // change code below this line
+
+
+
+  // change code above this line
+}
