85608119f6d027b8fdc1c701c3dc0768d60889e7
Dennis <deweis@gmx.ch>
2018-08-10 10:19:59 +0200

    Add "Render a Class Component to the DOM"

---
+++ 03 Front End Libraries/04 React/12 Render a Class Component to the DOM.js
@@ -0,0 +1,23 @@
+/**
+* Task: Both the Fruits and Vegetables components are defined for you behind the
+* scenes. Render both components as children of the TypesOfFood component, then
+* render TypesOfFood to the DOM. There is a div with id='challenge-node'
+* available for you to use.
+*/
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* change code below this line */}
+
+        {/* change code above this line */}
+      </div>
+    );
+  }
+};
+
+// change code below this line
