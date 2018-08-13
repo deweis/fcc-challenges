3c9684dc4c56ba06c635e80a77471e8171632499
Dennis <deweis@gmx.ch>
2018-08-13 10:46:03 +0200

    Solve "Use PropTypes to Define the Props You Expect"

---
+++ 03 Front End Libraries/04 React/18 Use PropTypes to Define the Props You Expect.js
@@ -0,0 +1,24 @@
+/**
+* Task: The code editor shows a ShoppingCart component. Define default props on
+* this component which specify a prop items with a value of 0.
+*/
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+};
+
+// change code below this line
+Items.propTypes = { quantity: PropTypes.number.isRequired };
+// change code above this line
+
+Items.defaultProps = {
+  quantity: 0
+};
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Items />
+  }
+};
