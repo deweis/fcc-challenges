e35fa975f720684d2c9dcd8da485679081df074f
Dennis <deweis@gmx.ch>
2018-08-13 10:38:09 +0200

    Add "Override Default Props"

---
+++ 03 Front End Libraries/04 React/17 Override Default Props.js
@@ -0,0 +1,22 @@
+/**
+* Task: The code editor shows a ShoppingCart component. Define default props on
+* this component which specify a prop items with a value of 0.
+*/
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+}
+
+Items.defaultProps = {
+  quantity: 0
+}
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    { /* change code below this line */ }
+    return <Items />
+    { /* change code above this line */ }
+  }
+};
