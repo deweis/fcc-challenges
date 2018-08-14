cf2f4b299b51783daef9169e66d59a7a25adac74
Dennis <deweis@gmx.ch>
2018-08-14 10:45:43 +0200

    Add "Access Props Using this.props"

---
+++ 03 Front End Libraries/04 React/19 Access Props Using this.props.js
@@ -0,0 +1,41 @@
+/**
+* Task: Render an instance of the ReturnTempPassword component in the parent
+* component ResetPassword. Here, give ReturnTempPassword a prop of tempPassword
+* and assign it a value of a string that is at least 8 characters long. Within
+* the child, ReturnTempPassword, access the tempPassword prop within the strong
+* tags to make sure the user sees the temporary password.
+*/
+class ReturnTempPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+            { /* change code below this line */ }
+            <p>Your temporary password is: <strong></strong></p>
+            { /* change code above this line */ }
+        </div>
+    );
+  }
+};
+
+class ResetPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+          <h2>Reset Password</h2>
+          <h3>We've generated a new temporary password for you.</h3>
+          <h3>Please reset this password from your account settings ASAP.</h3>
+          { /* change code below this line */ }
+
+          { /* change code above this line */ }
+        </div>
+    );
+  }
+};
