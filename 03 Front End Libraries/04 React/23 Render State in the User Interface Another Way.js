1cc752610619910d572e7f7479d2074d80756e32
Dennis <deweis@gmx.ch>
2018-08-15 09:42:00 +0200

    Solve "Render State in the User Interface Another Way"

--- 03 Front End Libraries/04 React/23 Render State in the User Interface Another Way.js
+++ 03 Front End Libraries/04 React/23 Render State in the User Interface Another Way.js
@@ -17,12 +17,12 @@ class MyComponent extends React.Component {
   }
   render() {
     // change code below this line
-
+    const name = this.state.name;
     // change code above this line
     return (
       <div>
         { /* change code below this line */ }
-
+        <h1>{name}</h1>
         { /* change code above this line */ }
       </div>
     );
