1cad45e68ca007ef8f821ef213c98a4fa0dcb5e3
Dennis <deweis@gmx.ch>
2018-08-21 11:26:59 +0200

    Solve "Create a Controlled Form"

--- 03 Front End Libraries/04 React/29 Create a Controlled Form.js
+++ 03 Front End Libraries/04 React/29 Create a Controlled Form.js
@@ -33,6 +33,10 @@ class MyForm extends React.Component {
   }
   handleSubmit(event) {
     // change code below this line
+    event.preventDefault();
+    this.setState({
+      submit: this.state.input,
+    });
 
     // change code above this line
   }
@@ -41,12 +45,12 @@ class MyForm extends React.Component {
       <div>
         <form onSubmit={this.handleSubmit}>
           { /* change code below this line */ }
-
+          <input value={this.state.input} onChange={this.handleChange} />
           { /* change code above this line */ }
           <button type='submit'>Submit!</button>
         </form>
         { /* change code below this line */ }
-
+        <h1>{this.state.submit}</h1>
         { /* change code above this line */ }
       </div>
     );
