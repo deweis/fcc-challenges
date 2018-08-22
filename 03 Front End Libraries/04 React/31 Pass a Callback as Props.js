0daa714116486cb96336a3da16e9dd999c1436ca
Dennis <deweis@gmx.ch>
2018-08-22 10:48:47 +0200

    Solve "Pass a Callback as Props"

--- 03 Front End Libraries/04 React/31 Pass a Callback as Props.js
+++ 03 Front End Libraries/04 React/31 Pass a Callback as Props.js
@@ -33,7 +33,8 @@ class MyApp extends React.Component {
     return (
        <div>
         { /* change code below this line */ }
-
+        <GetInput input={this.state.inputValue} handleChange={this.handleChange} />
+        <RenderInput input={this.state.inputValue} />
         { /* change code above this line */ }
        </div>
     );
