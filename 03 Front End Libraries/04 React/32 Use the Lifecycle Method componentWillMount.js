8a7949c5296d3e8b1718b32b6a1960e9e3aaa642
Dennis <deweis@gmx.ch>
2018-08-23 11:20:21 +0200

    Add "Use the Lifecycle Method componentWillMount"

---
+++ 03 Front End Libraries/04 React/32 Use the Lifecycle Method componentWillMount.js
@@ -0,0 +1,19 @@
+/**
+* Task: The componentWillMount() method is called before the render() method
+* when a component is being mounted to the DOM. Log something to the console
+* within componentWillMount() - you may want to have your browser console open
+* to see the output.
+*/
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // change code below this line
+
+    // change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
