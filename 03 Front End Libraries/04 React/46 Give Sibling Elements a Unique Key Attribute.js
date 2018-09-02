d39aaf05601ed8fc957f5a26569445bb77640b84
Dennis <deweis@gmx.ch>
2018-09-02 08:56:03 +0200

    Solve "Give Sibling Elements a Unique Key Attribute"

--- 03 Front End Libraries/04 React/46 Give Sibling Elements a Unique Key Attribute.js
+++ 03 Front End Libraries/04 React/46 Give Sibling Elements a Unique Key Attribute.js
@@ -21,7 +21,7 @@ const frontEndFrameworks = [
 ];
 
 function Frameworks() {
-  const renderFrameworks = null; // change code here
+  const renderFrameworks = frontEndFrameworks.map((item, index) => <li key={index}>{item}</li>);
   return (
     <div>
       <h1>Popular Front End JavaScript Frameworks</h1>
