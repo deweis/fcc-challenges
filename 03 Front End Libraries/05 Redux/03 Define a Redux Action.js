3d018b794921d0b1789e24d4d38dc58cec1fe6d7
Dennis <deweis@gmx.ch>
2018-09-03 09:31:09 +0200

    Add "Define a Redux Action"

---
+++ 03 Front End Libraries/05 Redux/03 Define a Redux Action.js
@@ -0,0 +1,6 @@
+/**
+* Task: Writing a Redux action is as simple as declaring an object with a type
+* property. Declare an object action and give it a property type set to the
+* string 'LOGIN'.
+*/
+// Define an action here:
