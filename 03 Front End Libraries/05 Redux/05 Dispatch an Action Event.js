3c692299b58067eee8cb37fddafa0f4dd5a86435
Dennis <deweis@gmx.ch>
2018-09-04 09:44:52 +0200

    Add "Dispatch an Action Event"

---
+++ 03 Front End Libraries/05 Redux/05 Dispatch an Action Event.js
@@ -0,0 +1,15 @@
+/**
+* Task: Define a function named actionCreator() that returns the action object
+* when called.
+*/
+const store = Redux.createStore(
+  (state = {login: false}) => state
+);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+// Dispatch the action here:
