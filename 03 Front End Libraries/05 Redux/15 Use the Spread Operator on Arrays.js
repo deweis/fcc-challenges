d3e868364adb8c0189848b6e3b383da04c5e6a7e
Dennis <deweis@gmx.ch>
2018-09-10 10:06:39 +0200

    Add "Use the Spread Operator on Arrays"

---
+++ 03 Front End Libraries/05 Redux/15 Use the Spread Operator on Arrays.js
@@ -0,0 +1,22 @@
+/**
+* Task: Use the spread operator to return a new copy of state when a to-do is
+*  added.
+*/
+const immutableReducer = (state = ['Do not mutate state!'], action) => {
+  switch(action.type) {
+    case 'ADD_TO_DO':
+      // don't mutate state here or the tests will fail
+      return
+    default:
+      return state;
+  }
+};
+
+const addToDo = (todo) => {
+  return {
+    type: 'ADD_TO_DO',
+    todo
+  }
+}
+
+const store = Redux.createStore(immutableReducer);
