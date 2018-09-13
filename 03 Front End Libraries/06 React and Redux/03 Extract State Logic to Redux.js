876943de72801b64482fe5953f2aca5cc2540cf7
Dennis <deweis@gmx.ch>
2018-09-13 09:42:40 +0200

    Add "Extract State Logic to Redux"

---
+++ 03 Front End Libraries/06 React and Redux/03 Extract State Logic to Redux.js
@@ -0,0 +1,8 @@
+/**
+* Task: Now that you finished the React component, you need to move the logic
+* it's performing locally in its state into Redux. This is the first step to
+* connect the simple React app to Redux. The only functionality your app has
+* is to add new messages from the user to an unordered list. The example is
+* simple in order to demonstrate how React and Redux work together.
+*/
+// define ADD, addMessage(), messageReducer(), and store here:
