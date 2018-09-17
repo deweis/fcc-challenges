097ce81e597c4b9155d10739219813d64cc363fd
Dennis <deweis@gmx.ch>
2018-09-17 10:29:11 +0200

    Solve "Map Dispatch to Props"

--- 03 Front End Libraries/06 React and Redux/06 Map Dispatch to Props.js
+++ 03 Front End Libraries/06 React and Redux/06 Map Dispatch to Props.js
@@ -13,3 +13,10 @@ const addMessage = (message) => {
 };
 
 // change code below this line
+function mapDispatchToProps(dispatch) {
+  return {
+    submitNewMessage: function(message) {
+      dispatch(addMessage(message));
+    }
+  };
+}
