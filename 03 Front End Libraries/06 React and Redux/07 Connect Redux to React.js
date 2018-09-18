1b90ef9f4529b13277b943f1597ce9488345f3de
Dennis <deweis@gmx.ch>
2018-09-18 09:41:51 +0200

    Solve "Connect Redux to React"

--- 03 Front End Libraries/06 React and Redux/07 Connect Redux to React.js
+++ 03 Front End Libraries/06 React and Redux/07 Connect Redux to React.js
@@ -38,4 +38,6 @@ class Presentational extends React.Component {
 };
 
 const connect = ReactRedux.connect;
+
 // change code below this line
+const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(Presentational);
