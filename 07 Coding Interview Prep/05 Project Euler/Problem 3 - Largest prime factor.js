b888d5fd8e725faa31bcd80455248abea5a08053
Dennis <deweis@gmx.ch>
2018-09-26 09:58:42 +0200

    Partial Solve "Problem 3: Largest prime factor"

--- 07 Coding Interview Prep/05 Project Euler/Problem 3 - Largest prime factor.js
+++ 07 Coding Interview Prep/05 Project Euler/Problem 3 - Largest prime factor.js
@@ -4,7 +4,10 @@
 What is the largest prime factor of the given number?
 */
 
-// Try making it recursive
+/*
+Version 1:
+--> Hanging at largestPrimeFactor(600851475143);
+*/
 
 function isPrime(num) {
   if (num < 2) return false;
@@ -33,3 +36,49 @@ function largestPrimeFactor(number) {
 
   return result;
 }
+
+/*
+Version 2: Try filling an array with all prime numbers and then get the max
+--> tbd
+*/
+
+/*
+Version 3: Make the function recursive
+--> Works fine for 13195 but getting "Potential infinite loop" error on 600851475143
+*/
+
+/* Recursive isPrime
+   divisor to be added is 2 in the beginning
+   I.e. isPrimeRec(16,2); */
+function isPrimeRec(num, divisor) {
+  if (num === divisor) {
+    return num;
+  } else if (num % divisor === 0) {
+    return false;
+  } else {
+    return isPrimeRec(num, divisor + 1);
+  }
+}
+
+function largestPrimeFactor(number) {
+  if (isPrimeRec(number, 2)) {
+    return number;
+  }
+
+  let result;
+  const half = number / 2;
+  for (let i = 2; i <= half; i++) {
+    if (isPrimeRec(i, 2) && number % i === 0) {
+      result = isPrimeRec(i, 2);
+    }
+  }
+
+  return result;
+}
+
+largestPrimeFactor(600851475143);
+
+/*
+Version 3: Make the largestPrimeFactor recursive as well
+--> 
+*/
