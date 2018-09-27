a6f8a9e94b0844b8a35eabb18bed10829f2e5eb6
Dennis <deweis@gmx.ch>
2018-09-27 09:51:44 +0200

    Partial Solve "Problem 4: Largest palindrome product"

--- 07 Coding Interview Prep/05 Project Euler/Problem 4 - Largest palindrome product.js
+++ 07 Coding Interview Prep/05 Project Euler/Problem 4 - Largest palindrome product.js
@@ -5,9 +5,44 @@ The largest palindrome made from the product of two 2-digit numbers is
 
 Find the largest palindrome made from the product of two n-digit numbers.
 */
+
+/*
+  Version 2: https://repl.it/repls/InstructiveAlertDriver
+  - Fill the max palindrome while reducing numA into an array.
+  - Add an outer loop to reduce numB and when numA has reached a palindrome,
+    reduce numB by 1 and go into the next round of numA
+*/
+
+/* Version 1: Works for n = 2
+      Issue: n = 3 returns */
 function largestPalindromeProduct(n) {
-  // Good luck!
-  return true;
+
+  // Checks if an input number is a palindrome
+  const isPalindrome = (num) => Number(String(num)
+                                        .split('')
+                                        .reverse()
+                                        .join('')) === num;
+
+  let numA = [];
+  let numB = [];
+
+  for (let i = 0; i < n; i++) {
+    numA.push(9);
+    numB.push(9);
+  }
+
+  numA = numA.join('');
+  numB = numB.join('');
+
+  let res;
+
+  for (let i = 1; i < numA; i++) {
+    if (isPalindrome((numA - i) * numB)) {
+      return res = (numA - i) * numB;
+    }
+  }
+
+  return res;
 }
 
 largestPalindromeProduct(3);
