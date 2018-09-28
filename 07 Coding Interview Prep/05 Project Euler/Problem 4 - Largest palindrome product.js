0c5f02ba538387d71f043371a1668bbe2d514a01
Dennis <deweis@gmx.ch>
2018-09-28 09:51:58 +0200

    Partial solve "Problem 4: Largest palindrome product"

--- 07 Coding Interview Prep/05 Project Euler/Problem 4 - Largest palindrome product.js
+++ 07 Coding Interview Prep/05 Project Euler/Problem 4 - Largest palindrome product.js
@@ -7,6 +7,50 @@ Find the largest palindrome made from the product of two n-digit numbers.
 */
 
 /*
+  Version 3: Replace the for loops with recursive functions
+*/
+function largestPalindromeProduct(n) {
+
+  /* Function to evaluate if a number is a palindrome */
+  const isPalindrome = (num) => Number(String(num).split('').reverse().join('')) === num;
+
+  /* define the two numbers based on n */
+  let numA = [];
+  let numB = [];
+
+  for (let i = 0; i < n; i++) {
+    numA.push(9);
+    numB.push(9);
+  }
+
+  numA = numA.join('');
+  numB = numB.join('');
+
+  /* loop through to find the palindromes */
+  let result = [];
+
+  function findPalindrome(num1, num2) {
+    if (num1 === 1) {
+      return;
+    } else if (isPalindrome(num1 * num2)) {
+      result.push(num1 * num2);
+      return;
+    } else {
+      findPalindrome(num1 - 1, num2);
+    }
+  }
+
+  for (let j = numB; j > 1; j--) {
+    findPalindrome(numA, j);
+  }
+
+  console.log(result.length);
+  return result.sort((a, b) => b - a)[0];
+}
+
+largestPalindromeProduct(3);
+
+/*
   Version 2: https://repl.it/repls/InstructiveAlertDriver
   - Fill the max palindrome while reducing numA into an array.
   - Add an outer loop to reduce numB and when numA has reached a palindrome,
