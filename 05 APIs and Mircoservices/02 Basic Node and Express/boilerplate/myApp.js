3a75e767d06ad6e2efcfddd837a294ae10cfb966
Dennis <deweis@gmx.ch>
2019-01-24 10:37:44 +0100

    06 Solve "Use the .env File"

--- 05 APIs and Mircoservices/02 Basic Node and Express/boilerplate/myApp.js
+++ 05 APIs and Mircoservices/02 Basic Node and Express/boilerplate/myApp.js
@@ -26,6 +26,9 @@ app.use(express.static(__dirname + '/public'));
 
 /** 5) serve JSON on a specific route */
 app.get('/json', function(req, res) {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    return res.json({ message: 'HELLO JSON' });
+  }
   res.json({ message: 'Hello json' });
 });
 
