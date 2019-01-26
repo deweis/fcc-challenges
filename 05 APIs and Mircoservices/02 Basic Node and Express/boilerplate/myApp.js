78dd85594821a66efa7e44911b0838c3695f390d
Dennis <deweis@gmx.ch>
2019-01-26 10:48:56 +0100

    07 Solve "Implement a Root-Level Request Logger Middleware"

--- 05 APIs and Mircoservices/02 Basic Node and Express/boilerplate/myApp.js
+++ 05 APIs and Mircoservices/02 Basic Node and Express/boilerplate/myApp.js
@@ -16,6 +16,13 @@ app.get('/', function(req, res) {
 });
 */
 
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+app.use(function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 /** 3) Serve an HTML file */
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -26,16 +33,12 @@ app.use(express.static(__dirname + '/public'));
 
 /** 5) serve JSON on a specific route */
 app.get('/json', function(req, res) {
+  /** 6) Use the .env file to configure the app */
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     return res.json({ message: 'HELLO JSON' });
   }
   res.json({ message: 'Hello json' });
 });
-
-/** 6) Use the .env file to configure the app */
-
-/** 7) Root-level Middleware - A logger */
-//  place it before all the routes !
 
 /** 8) Chaining middleware. A Time server */
 
