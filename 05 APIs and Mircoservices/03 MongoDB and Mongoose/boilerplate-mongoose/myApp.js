f6a66407925c949913f28dac7ef5e2e851add9c8
Dennis <deweis@gmx.ch>
2019-02-10 11:38:31 +0100

    07 Solve "Use model.findById() to Search Your Database By _id"

--- 05 APIs and Mircoservices/03 MongoDB and Mongoose/boilerplate-mongoose/myApp.js
+++ 05 APIs and Mircoservices/03 MongoDB and Mongoose/boilerplate-mongoose/myApp.js
@@ -160,7 +160,10 @@ var findOneByFood = function(food, done) {
 // Use the function argument 'personId' as search key.
 
 var findPersonById = function(personId, done) {
-  done(null /*, data*/);
+  Person.findById(personId, function(err, person) {
+    if (err) return console.error(err);
+    done(null, person);
+  });
 };
 
 /** # CR[U]D part III - UPDATE # 
