4b9486a5ab1ade4446871c6b150590430416f25e
Dennis <deweis@gmx.ch>
2019-02-12 11:19:48 +0100

    10 Solve "Delete One Document Using model.findByIdAndRemove"

--- 05 APIs and Mircoservices/03 MongoDB and Mongoose/boilerplate-mongoose/myApp.js
+++ 05 APIs and Mircoservices/03 MongoDB and Mongoose/boilerplate-mongoose/myApp.js
@@ -247,7 +247,10 @@ var findAndUpdate = function(personName, done) {
 // As usual, use the function argument `personId` as search key.
 
 var removeById = function(personId, done) {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, function(err, person) {
+    if (err) return console.error(err);
+    done(null, person);
+  });
 };
 
 /** 11) Delete many People */
