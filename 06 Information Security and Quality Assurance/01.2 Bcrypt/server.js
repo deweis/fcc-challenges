c2a1f6bfe05ae264cc30d3d457fb2b0b29a09934
Dennis <deweis@gmx.ch>
2019-03-18 10:28:32 +0100

    11 prettier server.js

--- 06 Information Security and Quality Assurance/01.2 Bcrypt/server.js
+++ 06 Information Security and Quality Assurance/01.2 Bcrypt/server.js
@@ -1,8 +1,8 @@
 'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const app = express();
 
 fccTesting(app); //For FCC testing purposes
 
@@ -10,18 +10,12 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
 
 //END_ASYNC
 
 //START_SYNC
 
-
-
 //END_SYNC
-
 
 app.listen(process.env.PORT || 3000, () => {});
