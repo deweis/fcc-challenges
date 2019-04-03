7ce8397b90f94ef1fd279ded9359f0f5292b660b
Dennis <deweis@gmx.ch>
2019-04-03 10:10:32 +0200

    Some refactoring based on proposed solution in lecture 7

--- 06 Information Security and Quality Assurance/03 Advanced Node and Express/server.js
+++ 06 Information Security and Quality Assurance/03 Advanced Node and Express/server.js
@@ -1,20 +1,48 @@
 'use strict';
 
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
 const session = require('express-session');
-const ObjectID = require('mongodb').ObjectID;
+const passport = require('passport');
 const mongo = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
+
+const app = express();
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongo.connect(process.env.DATABASE, (err, db) => {
   if (err) {
     console.log('Database error: ' + err);
   } else {
     console.log('Successful database connection');
+
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
+    });
+
+    passport.deserializeUser((id, done) => {
+      db.collection('users').findOne({ _id: new ObjectID(id) }, (err, doc) => {
+        done(null, doc);
+      });
+    });
 
     passport.use(
       new LocalStrategy(function(username, password, done) {
@@ -37,56 +65,30 @@ mongo.connect(process.env.DATABASE, (err, db) => {
       })
     );
 
-    passport.serializeUser((user, done) => {
-      done(null, user._id);
-    });
-
-    passport.deserializeUser((id, done) => {
-      db.collection('users').findOne({ _id: new ObjectID(id) }, (err, doc) => {
-        done(null, doc);
+    app.route('/').get((req, res) => {
+      res.render(process.cwd() + '/views/pug/index', {
+        title: 'Hello',
+        message: 'Please login',
+        showLogin: true
       });
     });
+
+    app.route('/profile').get((req, res) => {
+      res.render(process.cwd() + '/views/pug/profile');
+    });
+
+    app
+      .route('/login')
+      .post(
+        passport.authenticate('local', { failureRedirect: '/' }),
+        (req, res) => {
+          console.log(`User ${req.user} attempted to log in.`);
+          res.redirect('/');
+        }
+      );
 
     app.listen(process.env.PORT || 3000, () => {
       console.log('Listening on port ' + process.env.PORT);
     });
   }
 });
-
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true
-  })
-);
-
-app.set('view engine', 'pug');
-
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/index', {
-    title: 'Hello',
-    message: 'Please login',
-    showLogin: true
-  });
-});
-
-/*app.route('/profile').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/profile');
-});*/
-
-app
-  .route('/login')
-  .post(
-    passport.authenticate('local', { failureRedirect: '/' }),
-    (req, res) => {
-      console.log(`User ${req.user} attempted to log in.`);
-      res.redirect('/');
-    }
-  );
